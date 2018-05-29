3075bb4fa46258819a09eb41378f6b4affeb934d
Tomasz Kopacki <tomasz@kopacki.eu>
2018-05-29 12:27:59 +0200

    .

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,22 +1,11 @@
 import * as axios from 'axios';
-import Logger from 'node-logger-es6';
-
-let logger = Logger.configure(
-    {
-        level: 'debug',
-        rotation: 'd',
-        size: 5,
-        json: true,
-        timestamp: true
-    }
-);
 
 let gpioGetter = (ip, pin, onState) =>
     axios.post('http://' + ip + '/rpc/GPIO.Read', {
         "pin": pin
     })
     .then(response => {
-        logger.info(response);
+        console.log(response);
         return response.data.value === onState;
     });
 
@@ -25,7 +14,7 @@ let relayOn = (ip, channel) =>
         "name": channel
     })
     .then(response => {
-        logger.info(response);
+        console.log(response);
         return response.data.result === "ON"
     });
 
@@ -34,7 +23,7 @@ let relayOff = (ip, channel) =>
         "name": channel
     })
     .then(response => {
-        logger.info(response);
+        console.log(response);
         return response.data.result === "OFF"
     });
 
